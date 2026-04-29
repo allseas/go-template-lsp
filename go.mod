eb88a8a81ee4b6ead3dacd55f063fd62a46563c2
Bartosz Włodarczyk <bewuw@proton.me>
2026-04-29 10:21:19 +0200

    feat: stub language server

--- go.mod
+++
@@ -1,3 +0,0 @@
-module text-template-server
-
-go 1.26.1
---
+++ server/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/tliron/glsp"
+	protocol "github.com/tliron/glsp/protocol_3_16"
+	"github.com/tliron/glsp/server"
+)
+
+const lsName = "goTmpl"
+
+var (
+	version = "0.0.1"
+	handler protocol.Handler
+)
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	log.Print("starting server")
+
+	handler = protocol.Handler{
+		Initialize:  initialize,
+		Initialized: initialized,
+		Shutdown:    shutdown,
+		SetTrace:    setTrace,
+	}
+
+	lspServer := server.NewServer(&handler, lsName, false)
+
+	err := lspServer.RunStdio()
+	if err != nil {
+		log.Error().Err(err).Msg("error starting server")
+		return
+	}
+}
+
+func initialize(context *glsp.Context, params *protocol.InitializeParams) (any, error) {
+	log.Debug().Any("params", params).Msg("initializing")
+
+	capabilities := handler.CreateServerCapabilities()
+
+	return protocol.InitializeResult{
+		Capabilities: capabilities,
+		ServerInfo: &protocol.InitializeResultServerInfo{
+			Name:    lsName,
+			Version: &version,
+		},
+	}, nil
+}
+
+func initialized(context *glsp.Context, params *protocol.InitializedParams) error {
+	log.Debug().Any("params", params).Msg("initialized")
+
+	return nil
+}
+
+func shutdown(context *glsp.Context) error {
+	log.Debug().Msg("shutting down")
+
+	protocol.SetTraceValue(protocol.TraceValueOff)
+	return nil
+}
+
+func setTrace(context *glsp.Context, params *protocol.SetTraceParams) error {
+	log.Debug().Any("params", params).Msg("setting trace")
+	protocol.SetTraceValue(params.Value)
+	return nil
+}
