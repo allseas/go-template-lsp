af68fd666a3bec37e7fc2da61da51421012d0581
Bartosz Włodarczyk <bewuw@proton.me>
2026-05-06 13:16:56 +0200

    fix: format and lint

--- clients/JetBrains/go-text-template/src/main/kotlin/com/example/gotexttemplate/GoTextTemplateContext.kt
+++ clients/JetBrains/go-text-template/src/main/kotlin/com/example/gotexttemplate/GoTextTemplateContext.kt
@@ -4,7 +4,8 @@ import com.intellij.codeInsight.template.TemplateContextType
 import com.intellij.psi.PsiFile
 
 class GoTextTemplateContext : TemplateContextType("Go text/template") {
-    override fun isInContext(file: PsiFile, offset: Int): Boolean {
-        return file.name.endsWith(".tmpl")
-    }
+    override fun isInContext(
+        file: PsiFile,
+        offset: Int,
+    ): Boolean = file.name.endsWith(".tmpl")
 }
