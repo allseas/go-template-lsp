f71eac00ff69cedbb83d32b0536b5a1416ddd6fe
Bartosz Włodarczyk <bewuw@proton.me>
2026-05-06 15:52:30 +0200

    feat: created an icon for vsc and jebtrains

---
+++ clients/JetBrains/go-text-template/src/main/kotlin/com/example/gotexttemplate/TextTemplateIcons.kt
@@ -0,0 +1,9 @@
+package com.example.gotexttemplate
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object TextTemplateIcons {
+    @JvmField
+    val FileIcon: Icon = IconLoader.getIcon("/icons/icon.svg", javaClass)
+}
