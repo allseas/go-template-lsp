e73e408de6bf685d196a6549da78eba4174bb877
Bartosz Włodarczyk <bewuw@proton.me>
2026-06-04 10:26:27 +0200

    feat: go to model definiton on a field node

--- server/handlers/definition.go
+++ server/handlers/definition.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	gotypes "go/types"
+
 	parse "text-template-parser"
 
 	"github.com/rs/zerolog/log"
@@ -66,7 +68,78 @@ func Definition(_ *glsp.Context, params *protocol.DefinitionParams) (any, error)
 		}
 		return nil, nil
 	case parse.NodeField:
-		// TODO: go to the definition in the go files
+		if doc.loadedType == nil || doc.loadedType.Fset == nil || doc.loadedType.DotType == nil {
+			return nil, nil
+		}
+		target := node.(*parse.FieldNode)
+		if len(target.Ident) == 0 {
+			return nil, nil
+		}
+
+		// Determine which ident in the chain the cursor is on.
+		// A FieldNode at byte Pos covers ".Ident[0].Ident[1]..."
+		fieldOffset := int(target.Pos) + 1 // skip the leading '.'
+		targetIdx := len(target.Ident) - 1 // default to last ident
+		for i, ident := range target.Ident {
+			if i > 0 {
+				fieldOffset++ // skip the separator '.'
+			}
+			if offset >= fieldOffset && offset < fieldOffset+len(ident) {
+				targetIdx = i
+				break
+			}
+			fieldOffset += len(ident)
+		}
+
+		log.Debug().
+			Any("dotType", doc.loadedType.DotType).
+			Any("Ident", target.Ident).
+			Any("target", targetIdx).
+			Any("cursorPosition", offset).
+			Any("fieldNodePos", target.Pos).
+			Msg("definition NodeField")
+
+		// Walk the type chain to find the Go object at targetIdx.
+		var currentType gotypes.Type = doc.loadedType.DotType
+		for i := 0; i <= targetIdx; i++ {
+			obj, _, _ := gotypes.LookupFieldOrMethod(
+				currentType,
+				true,
+				doc.loadedType.Pkg,
+				target.Ident[i],
+			)
+			if obj == nil {
+				return nil, nil
+			}
+			if i == targetIdx {
+				pos := obj.Pos()
+				if !pos.IsValid() {
+					return nil, nil
+				}
+				fpos := doc.loadedType.Fset.Position(pos)
+				line := uint32(fpos.Line - 1)
+				char := uint32(fpos.Column - 1)
+				return protocol.Location{
+					URI: "file://" + fpos.Filename,
+					Range: protocol.Range{
+						Start: protocol.Position{Line: line, Character: char},
+						End:   protocol.Position{Line: line, Character: char},
+					},
+				}, nil
+			}
+			switch o := obj.(type) {
+			case *gotypes.Var:
+				currentType = o.Type()
+			case *gotypes.Func:
+				sig, ok := o.Type().Underlying().(*gotypes.Signature)
+				if !ok || sig.Results().Len() == 0 {
+					return nil, nil
+				}
+				currentType = sig.Results().At(0).Type()
+			default:
+				return nil, nil
+			}
+		}
 		return nil, nil
 	}
 
