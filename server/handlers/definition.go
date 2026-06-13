5984fde55f4247f4644622360ef5ecfe35cee819
Bartek Włodarczyk <bewuw@proton.me>
2026-06-13 16:11:39 +0200

    feat: use type tree for field node definition

--- server/handlers/definition.go
+++ server/handlers/definition.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	parse "text-template-parser"
+	serverTypes "text-template-server/types"
 
 	"github.com/rs/zerolog/log"
 	"github.com/tliron/glsp"
@@ -82,83 +83,105 @@ func Definition(_ *glsp.Context, params *protocol.DefinitionParams) (any, error)
 		}
 		return nil, nil
 	case *parse.FieldNode:
-		if loadedType == nil || loadedType.Fset == nil || loadedType.DotType == nil {
-			return nil, nil
-		}
-		if len(target.Ident) == 0 {
-			return nil, nil
-		}
-
-		targetIdx := getFieldIdentIdx(target, offset)
-
-		// log.Debug().
-		// 	Any("dotType", loadedType.DotType).
-		// 	Any("Ident", target.Ident).
-		// 	Any("target", targetIdx).
-		// 	Any("cursorPosition", offset).
-		// 	Any("fieldNodePos", target.Pos).
-		// 	Msg("definition NodeField")
-
-		var currentType gotypes.Type = loadedType.DotType
-		for i := 0; i <= targetIdx; i++ {
-			obj, _, _ := gotypes.LookupFieldOrMethod(
-				currentType,
-				true,
-				loadedType.Pkg,
-				target.Ident[i],
-			)
-			if obj == nil {
-				return nil, nil
-			}
-			if i == targetIdx {
-				pos := obj.Pos()
-				if !pos.IsValid() {
-					return nil, nil
-				}
-				fpos := loadedType.Fset.Position(pos)
-
-				var line uint32
-				var char uint32
-
-				if fpos.Line > 0 && fpos.Column > 0 {
-					line = uint32(fpos.Line - 1)   //nolint:gosec // disable G115
-					char = uint32(fpos.Column - 1) //nolint:gosec // disable G115
-				} else {
-					log.Debug().Any("fpos", fpos).Msg("Definition: fpos is not > 0")
-				}
-				filePath := filepath.ToSlash(fpos.Filename)
-				if len(filePath) > 0 && filePath[0] != '/' {
-					filePath = "/" + filePath
-				}
-
-				return protocol.Location{
-					URI: "file://" + filePath,
-					Range: protocol.Range{
-						Start: protocol.Position{Line: line, Character: char},
-						End:   protocol.Position{Line: line, Character: char},
-					},
-				}, nil
-			}
-			switch o := obj.(type) {
-			case *gotypes.Var:
-				currentType = o.Type()
-			case *gotypes.Func:
-				sig, ok := o.Type().Underlying().(*gotypes.Signature)
-				if !ok || sig.Results().Len() == 0 {
-					return nil, nil
-				}
-				currentType = sig.Results().At(0).Type()
-			default:
-				return nil, nil
-			}
-		}
-		return nil, nil
+		return definitionField(loadedType, target, offset, doc)
 	}
 
 	log.Debug().
 		Str("handler", "definition").
 		Any("node", node).
 		Msg("node at position is not a field or identifier")
+	return nil, nil
+}
+
+func definitionField(
+	loadedType *serverTypes.Tree,
+	target *parse.FieldNode,
+	offset int,
+	doc *document,
+) (any, error) {
+	if loadedType == nil || loadedType.Fset == nil {
+		return nil, nil
+	}
+	if len(target.Ident) == 0 {
+		return nil, nil
+	}
+
+	targetIdx := getFieldIdentIdx(target, offset)
+
+	// Resolve the dot type at the cursor position from the typed tree.
+	// This accounts for dot-context changes inside range/with blocks.
+	var currentType gotypes.Type
+	if typedTree := doc.typedTreeAt(
+		parse.Pos(offset),
+	); typedTree != nil &&
+		typedTree.Root != nil {
+		if typedNode := serverTypes.NodeFind(
+			typedTree.Root,
+			serverTypes.Pos(offset),
+		); typedNode != nil {
+			if enclosingList := serverTypes.EnclosingList(typedNode); enclosingList != nil {
+				currentType = enclosingList.ValueType()
+			}
+		}
+	}
+	if currentType == nil {
+		currentType = loadedType.DotType
+	}
+	if currentType == nil {
+		return nil, nil
+	}
+	for i := 0; i <= targetIdx; i++ {
+		obj, _, _ := gotypes.LookupFieldOrMethod(
+			currentType,
+			true,
+			loadedType.Pkg,
+			target.Ident[i],
+		)
+		if obj == nil {
+			return nil, nil
+		}
+		if i == targetIdx {
+			pos := obj.Pos()
+			if !pos.IsValid() {
+				return nil, nil
+			}
+			fpos := loadedType.Fset.Position(pos)
+
+			var line uint32
+			var char uint32
+
+			if fpos.Line > 0 && fpos.Column > 0 {
+				line = uint32(fpos.Line - 1)   //nolint:gosec // disable G115
+				char = uint32(fpos.Column - 1) //nolint:gosec // disable G115
+			} else {
+				log.Debug().Any("fpos", fpos).Msg("Definition: fpos is not > 0")
+			}
+			filePath := filepath.ToSlash(fpos.Filename)
+			if len(filePath) > 0 && filePath[0] != '/' {
+				filePath = "/" + filePath
+			}
+
+			return protocol.Location{
+				URI: "file://" + filePath,
+				Range: protocol.Range{
+					Start: protocol.Position{Line: line, Character: char},
+					End:   protocol.Position{Line: line, Character: char},
+				},
+			}, nil
+		}
+		switch o := obj.(type) {
+		case *gotypes.Var:
+			currentType = o.Type()
+		case *gotypes.Func:
+			sig, ok := o.Type().Underlying().(*gotypes.Signature)
+			if !ok || sig.Results().Len() == 0 {
+				return nil, nil
+			}
+			currentType = sig.Results().At(0).Type()
+		default:
+			return nil, nil
+		}
+	}
 	return nil, nil
 }
 
