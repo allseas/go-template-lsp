b08859b86c2eb3fcfaa2eb7528df47bd094fde97
Bartek Włodarczyk <bewuw@proton.me>
2026-06-13 18:19:42 +0200

    refactor: cleaned up the files

--- clients/JetBrains/go-text-template/src/test/kotlin/ExampleTest.kt
+++
@@ -1,7 +0,0 @@
-// For more info on myFixture. Read https://github.com/JetBrains/intellij-community/blob/idea/261.23567.138/platform/testFramework/src/com/intellij/testFramework/fixtures/CodeInsightTestFixture.java
-class ExampleTest : CustomPlatformTestCase() {
-    fun testTest() {
-        myFixture.configureByFile("exampleBefore.txt")
-        myFixture.checkResultByFile("exampleAfter.txt")
-    }
-}
