771a2122772709a96373a4e53b89660e76dd9953
Bartosz Włodarczyk <bewuw@proton.me>
2026-06-18 11:30:42 +0200

    feat: dont run benchmark on npm test
    (cherry picked from commit deeba42f3033561688147f35b81ff709552fba8d)

--- clients/VSCode/.vscode-test.mjs
+++ clients/VSCode/.vscode-test.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "@vscode/test-cli";
 
 const baseConfig = {
-    files: "out/test/**/*.test.js",
+    files: "out/test/!(benchmark).test.js",
     version: "stable",
     workspaceFolder: "../../test/resources/templ-tests",
     extensionDevelopmentPath: "./",
